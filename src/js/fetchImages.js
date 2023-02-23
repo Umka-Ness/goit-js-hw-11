d42a696215f5034000dd2fa9a63b8428b1a3eda9
Umka_Ness <melnik.dmitri2012@gmail.com>
2023-02-23 01:04:08 +0100

    update axios

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,5 +1,6 @@
+import axios from 'axios';
 export const fetchImages = async (inputValue, pageNr) => {
-  return await fetch(
+  return await axios.get(
     `https://pixabay.com/api/?key=29588079-fbc492831fdad231bf7222b96&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
   )
     .then(async response => {
